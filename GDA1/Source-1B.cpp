c3d698df34e3c4c31f130592732cf254c169cc08
Theodora Ferentinou <cs05379@uoi.gr>
2025-11-10 22:35:38 +0200

    meow

--- GDA1/Source-1B.cpp
+++ GDA1/Source-1B.cpp
@@ -48,7 +48,7 @@ glm::mat4 getProjectionMatrix() {
 
 void camera_function()
 {
-	
+
 }
 
 /////////////////////////////////////////////////
@@ -163,10 +163,10 @@ int main(void)
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	window = glfwCreateWindow(800, 800, "Project 1B template", NULL, NULL);
+	window = glfwCreateWindow(850, 850, u8"Εργασία 1Β - 2025 - Σκηνικό Παιχνιδιού", NULL, NULL);
 
 
 	if (window == NULL) {
@@ -178,7 +178,7 @@ int main(void)
 	glfwMakeContextCurrent(window);
 
 	// Initialize GLEW
-	glewExperimental = true; 
+	glewExperimental = true;
 	if (glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		getchar();
@@ -189,8 +189,8 @@ int main(void)
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	// background color
-	glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
+	// background color - dark gray
+	glClearColor(34.0f / 255.0f, 34.0f / 255.0f, 34.0f / 255.0f, 0.0f);
 	glEnable(GL_DEPTH_TEST);
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
@@ -202,115 +202,114 @@ int main(void)
 
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
-	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 4.0f, 0.1f, 100.0f);
+	glm::mat4 Projection = glm::perspective(glm::radians(80.0f), 4.0f / 4.0f, 0.1f, 100.0f);
 	// Camera matrix
 	glm::mat4 View = glm::lookAt(
-		glm::vec3(5.0f, 5.0f, 5.0f), // Camera in World Space
+		glm::vec3(0.0f, -5.0f, 20.0f), // Camera in World Space
 		glm::vec3(0.0f, 0.0f, 0.0f), // and looks at the origin
-		glm::vec3(0.0f, 0.0f, 1.0f));  // Head is up 
+		glm::vec3(0.0f, 1.0f, 0.0f));  // Head is up 
 	// Model matrix : an identity matrix (model will be at the origin)
 	glm::mat4 Model = glm::mat4(1.0f);
 	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 MVP = Projection * View * Model; 
-	
-    GLfloat len = 5.0f, wid=2.5f, heig=2.5f;
+	glm::mat4 MVP = Projection * View * Model;
 
-	static const GLfloat cube[] =
+	GLfloat len = 5.0f, wid = 2.5f, heig = 2.5f;
+
+	static const GLfloat charA[] =
 	{
-		-2.0f,-2.0f,-2.0f,
-		-2.0f,-2.0f, 2.0f,
-		-2.0f, 2.0f, 2.0f,
+		 0.0f,-7.75f,-1.0f, //v9
+		-1.5f,-8.5f ,-2.0f, //v8
+		-1.5f,-8.5f , 0.0f, //v4
 
-		 2.0f, 2.0f,-2.0f,
-		-2.0f,-2.0f,-2.0f,
-		-2.0f, 2.0f,-2.0f,
+		 0.0f,-7.75f,-1.0f, //v9
+		-1.5f,-8.5f , 0.0f, //v4
+		 1.5f,-8.5f, 0.0f,  //v3
 
-		 2.0f,-2.0f, 2.0f,
-		-2.0f,-2.0f,-2.0f,
-		 2.0f,-2.0f,-2.0f,
+		 0.0f,-7.75f,-1.0f, //v9
+		 1.5f,-8.5f, 0.0f,  //v3
+		 1.5f,-8.5f ,-2.0f, //v7
 
-		 2.0f, 2.0f,-2.0f,
-		 2.0f,-2.0f,-2.0f,
-		-2.0f,-2.0f,-2.0f,
+		 0.0f,-7.75f,-1.0f, //v9
+		 1.5f,-8.5f ,-2.0f, //v7
+		-1.5f,-8.5f ,-2.0f, //v8
 
-		-2.0f,-2.0f,-2.0f,
-		-2.0f, 2.0f, 2.0f,
-		-2.0f, 2.0f,-2.0f,
+		-1.5f,-8.5f ,-2.0f, //v8
+		-1.5f,-8.5f , 0.0f, //v4
+		-1.5f,-10.0f, 0.0f, //v1
 
-		 2.0f,-2.0f, 2.0f,
-		-2.0f,-2.0f, 2.0f,
-		-2.0f,-2.0f,-2.0f,
+		-1.5f,-8.5f ,-2.0f, //v8
+		-1.5f,-10.0f, 0.0f, //v1
+		-1.5f,-10.0f,-2.0f, //v5
 
-		-2.0f, 2.0f, 2.0f,
-		-2.0f,-2.0f, 2.0f,
-		 2.0f,-2.0f, 2.0f,
+		-1.5f,-8.5f ,-2.0f, //v8
+		-1.5f,-10.0f,-2.0f, //v5
+		 1.5f,-10.0f,-2.0f, //v6
 
-		 2.0f, 2.0f, 2.0f,
-		 2.0f,-2.0f,-2.0f,
-		 2.0f, 2.0f,-2.0f,
+		-1.5f,-8.5f ,-2.0f, //v8
+		 1.5f,-8.5f ,-2.0f, //v7
+		 1.5f,-10.0f,-2.0f, //v6
 
-		 2.0f,-2.0f,-2.0f,
-		 2.0f, 2.0f, 2.0f,
-		 2.0f,-2.0f, 2.0f,
+		 1.5f,-8.5f ,-2.0f, //v7
+		 1.5f,-10.0f,-2.0f, //v6
+		 1.5f,-10.0f, 0.0f, //v2
 
-		 2.0f, 2.0f, 2.0f,
-		 2.0f, 2.0f,-2.0f,
-		-2.0f, 2.0f,-2.0f,
+		 1.5f,-8.5f ,-2.0f, //v7
+		 1.5f,-10.0f, 0.0f, //v2
+		 1.5f,-8.5f, 0.0f,  //v3
 
-		 2.0f, 2.0f, 2.0f,
-		-2.0f, 2.0f,-2.0f,
-		-2.0f, 2.0f, 2.0f,
+		-1.5f,-8.5f , 0.0f, //v4
+		 1.5f,-10.0f, 0.0f, //v2
+		 1.5f,-8.5f, 0.0f,  //v3
 
-		 2.0f, 2.0f, 2.0f,
-		-2.0f, 2.0f, 2.0f,
-		 2.0f,-2.0f, 2.0f
+		-1.5f,-8.5f , 0.0f, //v4
+		 1.5f,-10.0f, 0.0f, //v2
+		-1.5f,-10.0f, 0.0f, //v1
 	};
 
-	GLfloat a=0.4f;
+	GLfloat a = 0.4f;
 	static const GLfloat color[] = {
-		0.583f,  0.771f,  0.014f,a,
-		0.609f,  0.115f,  0.436f,a,
-		0.327f,  0.483f,  0.844f,a,
-		0.822f,  0.569f,  0.201f,a,
-		0.435f,  0.602f,  0.223f,a,
-		0.310f,  0.747f,  0.185f,a,
-		0.597f,  0.770f,  0.761f,a,
-		0.559f,  0.436f,  0.730f,a,
-		0.359f,  0.583f,  0.152f,a,
-		0.483f,  0.596f,  0.789f,a,
-		0.559f,  0.861f,  0.639f,a,
-		0.195f,  0.548f,  0.859f,a,
-		0.014f,  0.184f,  0.576f,a,
-		0.771f,  0.328f,  0.970f,a,
-		0.406f,  0.615f,  0.116f,a,
-		0.676f,  0.977f,  0.133f,a,
-		0.971f,  0.572f,  0.833f,a,
-		0.140f,  0.616f,  0.489f,a,
-		0.997f,  0.513f,  0.064f,a,
-		0.945f,  0.719f,  0.592f,a,
-		0.543f,  0.021f,  0.978f,a,
-		0.279f,  0.317f,  0.505f,a,
-		0.167f,  0.620f,  0.077f,a,
-		0.347f,  0.857f,  0.137f,a,
-		0.055f,  0.953f,  0.042f,a,
-		0.714f,  0.505f,  0.345f,a,
-		0.783f,  0.290f,  0.734f,a,
-		0.722f,  0.645f,  0.174f,a,
-		0.302f,  0.455f,  0.848f,a,
-		0.225f,  0.587f,  0.040f,a,
-		0.517f,  0.713f,  0.338f,a,
-		0.053f,  0.959f,  0.120f,a,
-		0.393f,  0.621f,  0.362f,a,
-		0.673f,  0.211f,  0.457f,a,
-		0.820f,  0.883f,  0.371f,a,
-		0.982f,  0.099f,  0.879f,a,
-	}; 
+		0.255f,  0.192f,  0.203f,a, //v9 //pink
+		0.255f,  0.192f,  0.203f,a, //v8 
+		0.255f,  0.192f,  0.203f,a, //v4
+		0.255f,  0.255f,  0.000f,a, //v9 //yellow
+		0.255f,  0.255f,  0.000f,a, //v4
+		0.255f,  0.255f,  0.000f,a, //v3
+		0.000f,  0.255f,  0.000f,a, //v9 //green
+		0.000f,  0.255f,  0.000f,a, //v3
+		0.000f,  0.255f,  0.000f,a, //v7
+		0.128f,  0.000f,  0.128f,a, //v9 //purple
+		0.128f,  0.000f,  0.128f,a, //v7
+		0.128f,  0.000f,  0.128f,a, //v8
+		0.173f,  0.216f,  0.230f,a, //v8 //light blue
+		0.173f,  0.216f,  0.230f,a, //v4
+		0.173f,  0.216f,  0.230f,a, //v1
+		0.971f,  0.572f,  0.833f,a, //v8
+		0.140f,  0.616f,  0.489f,a, //v1
+		0.997f,  0.513f,  0.064f,a, //v5
+		0.945f,  0.719f,  0.592f,a, //v8
+		0.543f,  0.021f,  0.978f,a, //v5
+		0.279f,  0.317f,  0.505f,a, //v6
+		0.167f,  0.620f,  0.077f,a, //v8
+		0.347f,  0.857f,  0.137f,a, //v7
+		0.055f,  0.953f,  0.042f,a, //v6
+		0.714f,  0.505f,  0.345f,a, //v7
+		0.783f,  0.290f,  0.734f,a, //v6
+		0.722f,  0.645f,  0.174f,a, //v2
+		0.302f,  0.455f,  0.848f,a, //v7
+		0.225f,  0.587f,  0.040f,a, //v2
+		0.517f,  0.713f,  0.338f,a, //v3
+		0.053f,  0.959f,  0.120f,a, //v4
+		0.393f,  0.621f,  0.362f,a, //v2
+		0.673f,  0.211f,  0.457f,a, //v3
+		0.820f,  0.883f,  0.371f,a, //v
+		0.982f,  0.099f,  0.879f,a, //v
+	};
 
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
-	
+	glBufferData(GL_ARRAY_BUFFER, sizeof(charA), charA, GL_STATIC_DRAW);
+
 	GLuint colorbuffer;
 	glGenBuffers(1, &colorbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
@@ -320,21 +319,21 @@ int main(void)
 
 		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
+
 		// Use our shader
 		glUseProgram(programID);
 
-		glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 4.0f, 0.1f, 100.0f);
+		glm::mat4 Projection = glm::perspective(glm::radians(80.0f), 4.0f / 4.0f, 0.1f, 100.0f);
 		// Camera matrix
 		glm::mat4 View = glm::lookAt(
-			glm::vec3(5.0f, 5.0f, 5.0f), 
-			glm::vec3(0.0f, 0.0f, 0.0f), 
-			glm::vec3(0.0f, 0.0f, 1.0f) 
+			glm::vec3(0.0f, -5.0f, 20.0f),
+			glm::vec3(0.0f, 0.0f, 0.0f),
+			glm::vec3(0.0f, 1.0f, 0.0f)
 		);
-		
+
 		glm::mat4 Model = glm::mat4(1.0f);
-		
-		glm::mat4 MVP = Projection * View * Model; 
+
+		glm::mat4 MVP = Projection * View * Model;
 
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
@@ -342,37 +341,37 @@ int main(void)
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glVertexAttribPointer(
-			0,                  
-			3,                 
-			GL_FLOAT,           
-			GL_FALSE,           
-			0,                  
-			(void*)0            
+			0,
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
 		);
 
 		// 2nd attribute buffer : colors
 		glEnableVertexAttribArray(1);
 		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
 		glVertexAttribPointer(
-			1,                                
+			1,
 			4,                                // size
-			GL_FLOAT,                        
-			GL_FALSE,                         
-			0,                                
-			(void*)0                          
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
 		);
 
 		// Draw triangles 
-		glDrawArrays(GL_TRIANGLES, 0, 36); 
-		
+		glDrawArrays(GL_TRIANGLES, 0, 36); //12*3
+
 		glDisableVertexAttribArray(0);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
-	} // Check if the ESC key was pressed or the window was closed
-	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+	} // Check if the 1 key was pressed or the window was closed
+	while (glfwGetKey(window, GLFW_KEY_1) != GLFW_PRESS &&
 		glfwWindowShouldClose(window) == 0);
 
 	// Cleanup VBO
